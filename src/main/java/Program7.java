0d7b9617b0fcc5a898532398582f47890002cfef
aamirm10 <aamir.jan2008@gmail.com>
2024-09-06 23:02:54 -0700

    Update Program7.java

--- src/main/java/Program7.java
+++ src/main/java/Program7.java
@@ -1,30 +1,56 @@
 package src.main.java;
 //import stuff here
-
+import java.util.Scanner;
 //Your code here
 
 public class Program7{
-	//Define your final variables here
-	// **HINT: Maybe the conversion rates?????**
+    //Define your final variables here
+    // **HINT: Maybe the conversion rates?????**
 
+    final int klevinsInSchruteBucks = 20;
+    final int stanleyNickelsInKlevin = 12;    
 
-	public static void main(String[] args){
-		//Make you own test code here
-		Program7 tester = new Program7();
-		
-		double myTestVal = 0.0;
-		
-		myTestVal = tester.convertMoney(0,0,0);
-		
-		System.out.println(myTestVal);
-	}
-	
-	
-	
-	public double convertMoney(int schruteBucks, int stanleyNickels, int klevins){
-		//implement a function that converts from the old format to the new format
-		
-	}
-	
-	
+    public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+        
+        Program7 tester = new Program7();
+
+        System.out.println("Enter Schrute-Buck Amount: ");
+        double schruteBucks = myScanner.nextDouble();
+        
+        System.out.println("Enter Klevin Amount: ");
+        int klevins = myScanner.nextInt();
+        
+        System.out.println("Enter Stanley-Nickels Amount: ");
+        int stanleyNickels = myScanner.nextInt();
+        
+        double convertedValue = tester.convertMoney(schruteBucks, klevins, stanleyNickels);
+        System.out.println("Schrute-Bucks = $" + convertedValue);
+        
+
+        
+    }
+    
+    
+    
+    public double convertMoney(double schruteBucks, int klevins, int stanleyNickels){
+        double klevinsConverted = klevins + stanleyNickels / (double) stanleyNickelsInKlevin;
+        schruteBucks +=  klevinsConverted/ (double) klevinsInSchruteBucks;
+        schruteBucks = Math.round(100.0 * schruteBucks)/ 100.0;
+        
+        return schruteBucks;
+    }
+    
+    
 }
+
+/* paste console output
+Enter Schrute-Buck Amount: 
+7
+Enter Klevin Amount: 
+17
+Enter Stanley-Nickels Amount: 
+9
+Schrute-Bucks = $7.89
+
+ */
